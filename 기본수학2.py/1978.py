eaf2339d76432f6ccfa8ab2e25d921111b99e832
Gamcho <terrypark9812@naver.com>
2022-04-26 00:16:39 +0900

    리스트에 요소 추가 및 제거하는 법 / 백준.py 1978

---
+++ 기본수학2.py/1978.py
@@ -0,0 +1,19 @@
+n = int(input())
+
+numbers = map(int, input().split())
+sosu = 0
+
+
+for number in numbers:
+    error = 0
+    if number == 1:     # 1은 소수가 아님.   
+        pass
+    else:               # 2이상의 자연수에 대해서
+        for i in range(2, number):  # 2 ~ number-1
+            if number % i == 0:
+                error += 1
+                break
+        if error == 0:
+            sosu += 1
+
+print(sosu)     
--- 복습할것/exam.py
+++ 복습할것/exam.py
@@ -1,16 +1,8 @@
-n = int(input())        # 설탕
-# n = 5*a + 3*b
+a = []
+for i in range(10):
+    if i % 2 == 1:
+        pass
+    if i in range(10):
+        a.append(i)
 
-result = 0
-
-while n >= 0:
-    if n % 5 == 0:
-        print(result)
-        break
-    else:
-        result += 1
-        n -= 3
-else:
-    print(-1)
-
-
+print(a)
